9234ca63442ec59626334f36299bba71f7110ccf
pomodoro99 <jpomodoro99@gmail.com>
2022-05-20 17:35:12 +0900

    네이버api 구현1

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,104 @@
 <template>
-  <div>홈</div>
+  <v-container>
+    <v-row>
+
+      <v-col
+        cols="12"
+        sm="6"
+      >
+        <v-text-field
+          type="text"
+          v-model="search"
+          @keyup.enter="callData"
+        ></v-text-field>
+      </v-col>
+      <v-col
+        cols="12"
+        sm="2"
+      >
+        <v-btn
+          color="blue-grey"
+          class="ma-2 white--text"
+          @click="callData"
+        >네이버 검색</v-btn>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col
+        cols="12"
+        sm="8"
+      >
+        <v-card
+          elevation="2"
+          outlined
+          shaped
+          tile
+          v-for="(item, index) in list"
+          :key="index"
+          style="margin:5px"
+        >
+          <div class="d-flex flex-no-wrap justify-space-between">
+            <div
+              align="left"
+              @click="onClickRedirect(item.url)"
+            >
+              <v-card-title
+                class="text-h5"
+                v-html="item.title"
+              ></v-card-title>
+              <v-card-subtitle v-html="item.contents"></v-card-subtitle>
+            </div>
+            <v-avatar
+              class="ma-3"
+              size="100"
+              tile
+            >
+              <v-img :src="item.thumbnail"></v-img>
+            </v-avatar>
+          </div>
+        </v-card>
+      </v-col>
+    </v-row>
+
+  </v-container>
+
 </template>
 
 <script>
+import axios from "axios";
 
-  export default {
-    name: 'Home',
+export default {
+  name: 'Home',
 
-    components: {
-      
+  data: () => ({
+    search: '',
+
+    list: []
+  }),
+
+  components: {
+
+  },
+
+
+  methods: {
+    callData () {
+      axios.get("/v1/search/webkr.json", {      //axios의 결과가  promise이다
+        headers: {
+          "X-Naver-Client-Id": "8x1NIuLCel90bvpW_mwb",
+          "X-Naver-Client-Secret": "hI7_xn7PSo"
+        },
+        params: {
+          query: this.search
+        }
+      }).then(response => {
+        console.log(response)
+        //this.list = response.data.documents;
+      }).catch(error => {
+        console.error(error)
+      })
     },
+
   }
+}
 </script>
--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,13 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+  devServer: {
+    proxy: {
+      '/v1': {  // v1으로 시작하는 api주소를 target으로 proxy한다. 
+        target: 'https://openapi.naver.com/',
+        changeOrigin: true
+      }
+    }
+  }
 })
